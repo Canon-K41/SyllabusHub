44068eefce986fea8055f74ada3aa57d1c7b4625
Kensuke-K41 <kai.kensuke.255@gmail.com>
2024-09-22 21:15:40 +0900

    mod-sidbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,80 +2,74 @@
 
 import "../sytles/globals.css";
 import { useState, useEffect } from 'react'
-import IconToggle from '../assets/icons/square-toggle.svg'
-import IconHome from '../assets/icons/home.svg'
-import IconSettings from '../assets/icons/setting.svg'
-import IconHelp from '../assets/icons/help.svg'
+import IconToggle from '@/assets/icons/square-toggle.svg'
+import IconHome from '@/assets/icons/home.svg'
+import IconCalendar from '@/assets/icons/week-calendar.svg'
+import IconSettings from '@/assets/icons/setting.svg'
+import IconHelp from '@/assets/icons/help.svg'
 import Link from 'next/link'
 import clsx from 'clsx'
 
+const links = [
+  { href: '/', label: 'ホーム', icon: <IconHome className='inline-block mr-2' /> },
+  { href: '/weekcalendar', label: '時間割', icon: <IconCalendar className='inline-block mr-2' /> },
+  { href: '/settings', label: '設定', icon: <IconSettings className='inline-block mr-2' /> },
+  { href: '/help', label: 'ヘルプ', icon: <IconHelp className='inline-block mr-2' /> },
+];
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const [isOpen, setIsOpen] = useState(false)
-      useEffect(() => {
-        const handleResize = () => {
-          setIsOpen(window.innerWidth >= 768)
-        }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-        window.addEventListener('resize', handleResize)
-        handleResize()
+  useEffect(() => {
+    const handleResize = () => {
+      setIsOpen(window.innerWidth >= 768);
+    };
 
-        return () => window.removeEventListener('resize', handleResize)
-      }, [])
+    window.addEventListener('resize', handleResize);
+    handleResize();
 
-      return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel='manifest' href='manifest.json' />
-          <title>title</title>
-        </head>
-        <body>
-          <div className="min-h-screen bg-gray-100">
-            <button
-            className="p-2 fixed top-0 left-0 m-4 bg-white rounded shadow-lg z-10"
-            onClick={() => setIsOpen(!isOpen)}
-          ><IconToggle className='text-gray-700' />
-            </button>
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-              <div
-              className={clsx('fixed top-0 left-0 h-full w-60 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-10 md:translate-x-0', isOpen ? 'translate-x-0 ' : 'hidden' )}
-            >
-                <nav className="p-2 space-y-2">
-                  <button
-                  className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded transition duration-200"
-                  onClick={() => setIsOpen(!isOpen)}
-                ><IconToggle className='inline-block mr-2' />
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel='manifest' href='manifest.json' />
+        <title>title</title>
+      </head>
+      <body>
+        <div className="min-h-screen bg-gray-100">
 
-                </button>
-                <div className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded transition duration-200" >
-                  <Link href="#" />
-                  <IconHome className='inline-block mr-2' />
-                  ホーム
+          <div
+            className={clsx('fixed top-0 left-0 h-full w-60 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-10 md:translate-x-0', isOpen ? 'translate-x-0 ' : 'hidden')}
+          >
+            <nav className="p-2 space-y-2">
+              {links.map((link, index) => (
+                <div key={index} className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded transition duration-200">
+                  <Link href={link.href}>
+                    {link.icon}
+                    {link.label}
+                  </Link>
                 </div>
-                <div className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded transition duration-200">
-                  <Link href="#" />
-                  <IconSettings className='inline-block mr-2' />
-                  設定
-                </div>
-                <div className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded transition duration-200">
-                  <Link href="#" />
-                  <IconHelp className='inline-block mr-2' />
-                  ヘルプ
-                </div>
-                </nav>
-              </div>
-
-              <main className={clsx('mt-9 p-5 transition-all duration-200', isOpen ? 'ml-64' : ' ')}>
-              {children}
-              </main>
+              ))}
+            </nav>
           </div>
-        </body>
-      </html>
-      )
+
+          <main className={clsx('transition-all duration-200', isOpen ? 'ml-60' : '')}>
+            <header className="flex h-16 bg-white shadow items-center">
+              <button
+              className='p-3 rounded-md hover:bg-gray-200 '
+              onClick={() => setIsOpen(!isOpen)}
+            >
+                <IconToggle className='text-gray-700' />
+              </button>
+             </header>
+            {children}
+          </main>
+        </div>
+      </body>
+    </html>
+  );
 }
