b4ab74b36ec4a6ae03a978b95c62e20b511e5594
kensuke-K41 <kai.kensuke255.@gmail.com>
2024-10-06 16:29:26 +0900

    mod-classenventErrer

--- src/hooks/useClassEvents.ts
+++ src/hooks/useClassEvents.ts
@@ -8,25 +8,29 @@ const useClassEvents = () => {
   const loadClassEvents = useCallback(async () => {
     const classes = await getClass();
     const classEvents = classes.map((classData) => {
-      return {
-        class_id: classData.class_id,
-        title: classData.title,
-        url: classData.url,
-        duration: classData.duration,
-        instructor: classData.instructor,
-        university: classData.university,
-        description: classData.description,
-        exdata: classData.exdata,
-        rrule: {
-          freq: classData.rrule.freq,
-          interval: classData.rrule.interval,
-          count: classData.rrule.count,
-          byweekday: classData.rrule.byweekday,
-          dtstart: classData.rrule.dtstart + ':00',
-        },
-      };
-    });
-    setClassEvents(classEvents);
+      if (classData.rrule && classData.rrule.freq) {
+        return {
+          class_id: classData.class_id,
+          title: classData.title,
+          url: classData.url,
+          duration: classData.duration,
+          instructor: classData.instructor,
+          university: classData.university,
+          description: classData.description,
+          exdata: classData.exdata,
+          rrule: {
+            freq: classData.rrule.freq,
+            interval: classData.rrule.interval,
+            count: classData.rrule.count,
+            byweekday: classData.rrule.byweekday,
+            dtstart: classData.rrule.dtstart + ':00',
+          },
+        };
+      }
+      return null; // Return null for invalid objects
+    }).filter(event => event !== null); // Filter out null values
+
+    setClassEvents(classEvents as classData[]);
   }, []);
 
   useEffect(() => {
